40e23f27471018a5bc484e452727daf59ad53c17
Fraz Engineering <frazvisuals@gmail.com>
2018-11-17 09:49:39 +0100

    Add licence text

--- sanitizeme.go
+++ sanitizeme.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2018, github.com/frazrepo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -9,13 +23,6 @@ import (
 )
 
 func main() {
-
-	//Debug
-	// sanitizer := services.Sanitizer{}
-	// filename1 := os.Args[1]
-	// fmt.Println(sanitizer.Sanitize(filename1))
-	// os.Exit(0)
-	// End Debug
 
 	if len(os.Args) < 2 {
 		fmt.Println("File is required")
